c9dbb55d2904015eaf6cd3cd33903e190a211ac8
Karthick <kaarthy.kat13@teezle.com>
2021-08-25 13:05:28 +0530

    Update h8-elements.js

--- plugins/h8-elements.js
+++ plugins/h8-elements.js
@@ -2,5 +2,11 @@ import Vue from 'vue';
 // const { Btn } = require('@tkweb/elm').default
 import H8UiElements from "@tkweb/ui-elements"
 
+/** @ Usage in Vue component */
+// import { Btn, Badge } from "@tkweb/ui-elements"
+// components: {
+//     Btn,
+//     Badge
+// }
+
 Vue.use(H8UiElements)
-// Vue.component('Btn', Btn);
